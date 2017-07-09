e6ae1ccd57d51960ea78989b0cf6a283cf7089d3
nanaeaubry <nanaeaubry@gmail.com>
2017-07-09 07:55:19 -0700

    added user

--- scripts/example.js
+++ scripts/example.js
@@ -1,67 +1,89 @@
 module.exports = function(robot) {
 
-  robot.hear(/alright/, function(res) {
-    return res.send('mmhmmmm');
-  });
-
-  var lulz = ['lol', 'rofl', 'lmao'];
-  robot.respond(/lol/i, function(res) {
-    return res.send(res.random(lulz));
-  });
-
+  // When robot hears Hello it sends message
   robot.hear(/Hello/i, function(res) {
     return res.send('Hey girl Hey!');
   });
-
+  // If user sends direct message to robot then it will respond by randomly
+  // choosing response from array.
+  var laugh = ['lol', 'you funny', 'hilarious'];
+  robot.respond(/lol/i, function(res) {
+    return res.send(res.random(laugh));
+  });
+  // When robot hears sure it sends message
+  robot.hear(/sure/, function(res) {
+    return res.send('mmhmmmm');
+  });
+  // When robot hears sad it sends message
   robot.hear(/sad/i, function(res) {
     return res.send("If it don't pay your bills, then pay it no mind");
   });
-
+  // When robot hears go it sends message
   robot.hear(/go/i, function(res) {
     return res.send('Start your engines');
   });
-
+  // If group changes topic in chat then automatic messae will be delivered.
+  // Taken from example_scripts.js and changed the message.
   robot.topic(function(res) {
     return res.send(res.message.text + "? Where are we going with this?");
   });
+
+
+  // Taken from example_scripts.js in class repo and added words of my own.
+  // When users enter or leave a chat room then bot will say something in
+  // general to group from array of choices.
   enterReplies = ['Hi', 'Target Acquired', 'Hello friend.', 'I see you'];
   leaveReplies = ['Are you still there?', 'Bye Felicia', 'Wait what'];
 
   robot.enter(function(res) {
-    return res.send(res.random(enterReplies));
+    return res.send(res.random(enterReplies)); // Takes random reply from array
   });
 
   robot.leave(function(res) {
-    return res.send(res.random(leaveReplies));
+    return res.send(res.random(leaveReplies)); // Takes random reply from array
   });
 
-  robot.respond(/you are a little slow/, function(res) {
-    return setTimeout(function() {
-      return res.send("Who you calling 'slow'?");
-    }, 6 * 1000);
-  });
-
+  // Create an interval Id for conditional statement
   canIntervalId = null;
 
+  // When someone types 'I cant' condition should be executed
   robot.respond(/I cant/, function(res) {
+    // if the intervalId is not null then the following response should be given
     if (canIntervalId !== null) {
       res.send("Believe in yourself!");
       return;
     }
+    // if this is first time executing command or IntervalId is null then
+    //following response should be given
     res.send("You don't really think that?");
     return canIntervalId = setInterval(function() {
       return res.send("Believe in yourself");
-    }, 1000);
-    canIntervalId = 1;
+    }, 1000); //response will be given 1000 times
+    canIntervalId = 1; // set IntervalId to 1 so no longer null
   });
 
+  // Ends repetition in conditional with same intervalId
   robot.respond(/I will/, function(res) {
+    // if the intervalId is not null then the following response should be given
     if (canIntervalId !== null) {
       res.send("Yay! Course you can!");
       clearInterval(canIntervalId);
       return canIntervalId = null;
-    } else {
+    }
+    // if this is first time executing command or IntervalId is null then
+    //following response should be given
+    else {
       return res.send("Isn't that what I told you??");
+    }
+  });
+
+  robot.respond(/Hi! My name is (.*)/i, function(res) {
+    var name;
+    name = res.match[1];
+    if (name === "Rubot") {
+      return res.send("Say what? No way, that's me!");
+    } else {
+      return res.send("Pleasure to meet you " + name + "!");
     }
   });
 
