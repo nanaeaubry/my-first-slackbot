9e3612abc78a96b1be412252cecc63d60505bf43
nanaeaubry <nanaeaubry@gmail.com>
2017-07-09 11:12:35 -0700

    wip

--- scripts/example.js
+++ scripts/example.js
@@ -78,7 +78,7 @@ module.exports = function(robot) {
   });
   //class = ['carenbraj', 'dafabulousteach', 'jason', 'mennake', 'acervantes', 'nanaeaubry'];
   robot.respond(/Gak/i, function(res) {
-    robot.messageRoom('nanaeaubry', res.envelope.user.name + ' used the Gak command');
+    robot.messageRoom('nanaeplayground', 'Someone used the Gak command');
     return res.send('Goink goink!');
     // var name;
     // name = msg.match[1];
